519c95e198fc7b366c4424d5138f2dd061fd0fd4
Jake Donham <jake@donham.org>
2010-05-20 13:35:18 -0400

    more Element methods

--- src/ojox/element.ml
+++ src/ojox/element.ml
@@ -23,6 +23,8 @@ class type c =
 object
   inherit Node.c
 
+  method blur : unit
+  method focus : unit
   method _get_className : string
   method _get_clientHeight : string
   method _get_clientWidth : string
@@ -32,7 +34,7 @@ object
   method _get_lang : string
   method _get_offsetParent : c
   method _get_style : Style.c
-  method _get_tagName : string
+  method _get_tabIndex : int
   method _get_title : string
   method removeAttribute : string -> unit
   method setAttribute : string -> string -> unit
@@ -59,26 +61,141 @@ let as_node o =
   assert (is_node o);
   (Obj.magic o : c)
 
+let addClassName e className =
+  let className = JavaString.trim className in
+  if JavaString.length className = 0 then failwith "Unexpectedly empty class name";
+
+  (* Get the current style string. *)
+  let oldClassName = ref e#_get_className in
+  let idx = ref (JavaString.indexOf !oldClassName className) in
+
+  (* Calculate matching index. *)
+  let continue = ref true in
+  while !idx != -1 && !continue do
+    if !idx = 0 || JavaString.charAt !oldClassName (!idx - 1) = ' ' then begin
+      let last = !idx + JavaString.length className in
+      let lastPos = JavaString.length !oldClassName in
+      if last = lastPos || (last < lastPos && JavaString.charAt !oldClassName last = ' ')
+      then continue := false
+    end;
+    if !continue then idx := JavaString.indexOf !oldClassName ~startIndex:(!idx + 1) className
+  done;
+
+  if !idx = -1
+  then begin
+    if JavaString.length !oldClassName > 0
+    then oldClassName := !oldClassName ^ " ";
+    e#_set_className (!oldClassName ^ className)
+  end  
+
+let getAbsoluteLeft e = failwith "unimplemented"
+  (* DOMImpl.impl.getAbsoluteLeft(this); *)
+
+let getAbsoluteTop e = failwith "unimplemented"
+  (* DOMImpl.impl.getAbsoluteTop(this); *)
+
+let getAttribute e name = failwith "unimplemented"
+  (* DOMImpl.impl.getAttribute(this, name); *)
+
+let getFirstChildElement e = failwith "unimplemented"
+  (* DOMImpl.impl.getFirstChildElement(this); *)
+
+let getInnerHTML e = failwith "unimplemented"
+  (* DOMImpl.impl.getInnerHTML(this); *)
+
+let getInnerText e = failwith "unimplemented"
+  (* DOMImpl.impl.getInnerText(this); *)
+
+let getNextSiblingElement e = failwith "unimplemented"
+  (* DOMImpl.impl.getNextSiblingElement(this); *)
+
 let getOffsetHeight e = << $e$.offsetHeight || 0 >>
 let getOffsetLeft e = << $e$.offsetLeft || 0 >>
 let getOffsetTop e = << $e$.offsetTop || 0 >>
 let getOffsetWidth e = << $e$.offsetWidth || 0 >>
 
+let getAbsoluteBottom e = getAbsoluteTop e + getOffsetHeight e
+let getAbsoluteRight e = getAbsoluteLeft e + getOffsetWidth e
+
 let getPropertyBoolean e name = << !!$e$[$name$] >>
 let getPropertyDouble e name =  << parseFloat($e$[$name$]) || 0.0 >>
 let getPropertyInt e name =  << parseInt($e$[$name$]) || 0 >>
+let getPropertyJSO e name = << $e$[$name$] || null >>
 let getPropertyString e name =  << ($e$[$name$] == null) ? null : String($e$[$name$]) >>
 
 let getScrollHeight e = << $e$.scrollHeight || 0 >>
+
+let getScrollLeft e = failwith "unimplemented"
+  (* DOMImpl.impl.getScrollLeft(this); *)
+
 let getScrollTop e = << $e$.scrollTop || 0 >>
 let getScrollWidth e = << $e$.scrollWidth || 0 >>
 
+let getString e = failwith "unimplemented"
+  (* DOMImpl.impl.toString(this); *)
+
+let getTagName e = failwith "unimplemented"
+  (* return DOMImpl.impl.getTagName(this); *)
+
+let hasAttribute e name = failwith "unimplemented"
+  (* return DOMImpl.impl.hasAttribute(this, name); *)
+
+let hasTagName e tagName = tagName = getTagName e
+
+let removeClassName e className =
+  let className = JavaString.trim className in
+  if JavaString.length className = 0 then failwith "Unexpectedly empty class name";
+
+  (* Get the current style string. *)
+  let oldStyle = e#_get_className in
+  let idx = ref (JavaString.indexOf oldStyle className) in
+
+  (* Calculate matching index. *)
+  let continue = ref true in
+  while !idx != -1 && !continue do
+    if !idx = 0 || JavaString.charAt oldStyle (!idx - 1) = ' '
+    then begin
+      let last = !idx + JavaString.length className in
+      let lastPos = JavaString.length oldStyle in
+      if last = lastPos || (last < lastPos && JavaString.charAt oldStyle last = ' ')
+      then continue := false
+    end;
+    if !continue then idx := JavaString.indexOf oldStyle ~startIndex:(!idx + 1) className
+  done;
+
+  (* Don't try to remove the style if it's not there. *)
+  if !idx != -1 then begin
+    (* Get the leading and trailing parts, without the removed name. *)
+    let begin_ = JavaString.trim (JavaString.substring ~endIndex:!idx oldStyle 0) in
+    let end_ = JavaString.trim (JavaString.substring oldStyle (!idx + JavaString.length className)) in
+
+    (* Some contortions to make sure we don't leave extra spaces. *)
+    let newClassName =
+      if JavaString.length begin_ = 0 then end_
+      else if JavaString.length end_ = 0 then begin_
+      else begin_ ^ " " ^ end_ in
+    e#_set_className newClassName
+  end
+
+let replaceClassName e oldClassName newClassName =
+  removeClassName e oldClassName;
+  addClassName e newClassName
+
+let scrollIntoView e = failwith "unimplemented"
+  (* DOMImpl.impl.scrollIntoView(this); *)
+
 let setInnerHTML e html = <:stmt< ($e$).innerHTML = $html$ || ''; >>
+let setInnerText e text = failwith "unimplemented"
+  (* DOMImpl.impl.setInnerText(this, text); *)
 
 let setPropertyBoolean e name value = <:stmt< ($e$)[$name$] = $value$; >>
 let setPropertyDouble e name value = <:stmt< ($e$)[$name$] = $value$; >>
 let setPropertyInt e name value = <:stmt< ($e$)[$name$] = $value$; >>
+let setPropertyJSO e name value = <:stmt< ($e$)[$name$] = $value$; >>
 let setPropertyString e name value = <:stmt< ($e$)[$name$] = $value$; >>
+
+let setScrollLeft e scrollLeft = failwith "unimplemented"
+  (* DOMImpl.impl.setScrollLeft(this, scrollLeft) *)
 
 let setTitle e title =
   (* Setting the title to null results in the string "null" being displayed
--- src/ojox/element.mli
+++ src/ojox/element.mli
@@ -22,6 +22,16 @@ object
   inherit Node.c
 
   (**
+     Removes keyboard focus from this element.
+  *)
+  method blur : unit
+
+  (**
+     Gives keyboard focus to this element.
+  *)
+  method focus : unit
+
+  (**
      The class attribute of the element. This attribute has been renamed due to
      conflicts with the "class" keyword exposed by many languages.
      
@@ -86,9 +96,11 @@ object
   method _get_style : Style.c
 
   (**
-     The name of the element.
-   *)
-  method _get_tagName : string
+     The index that represents the element's position in the tabbing order.
+     
+     @see <http://www.w3.org/TR/1999/REC-html401-19991224/interact/forms.html#adef-tabindex> W3C HTML Specification
+  *)
+  method _get_tabIndex : int
 
   (**
      The element's advisory title.
@@ -130,6 +142,8 @@ object
    *)
   method _set_id : string -> unit
 
+
+
   (**
      Language code defined in RFC 1766.
    *)
@@ -166,6 +180,73 @@ val is_javaScriptObject : JavaScriptObject.c -> bool
    [false].
  *)
 val is_node : Node.c -> bool
+
+(**
+   Adds a name to this element's class property. If the name is already
+   present, this method has no effect.
+   
+   @param className the class name to be added
+   see {!#setClassName(String)}
+*)
+val addClassName : c -> string -> unit
+
+(**
+   Gets an element's absolute bottom coordinate in the document's coordinate
+   system.
+*)
+val getAbsoluteBottom : c -> int
+
+(**
+   Gets an element's absolute left coordinate in the document's coordinate
+   system.
+*)
+val getAbsoluteLeft : c -> int
+
+(**
+   Gets an element's absolute top coordinate in the document's coordinate
+   system.
+*)
+val getAbsoluteTop : c -> int
+
+(**
+   Gets an element's absolute right coordinate in the document's coordinate
+   system.
+*)
+val getAbsoluteRight : c -> int
+
+(**
+   Retrieves an attribute value by name.  Attribute support can be
+   inconsistent across various browsers.  Consider using the accessors in
+   {!Element} and its specific subclasses to retrieve attributes and
+   properties.
+   
+   @param name The name of the attribute to retrieve
+   @return The Attr value as a string, or the empty string if that attribute
+           does not have a specified or default value
+*)
+val getAttribute : c -> string -> string
+
+(**
+   The first child of element this element. If there is no such element, this
+   returns null.
+*)
+val getFirstChildElement : c -> c
+
+(**
+  All of the markup and content within a given element.
+*)
+val getInnerHTML : c -> string
+
+(**
+   The text between the start and end tags of the object.
+*)
+val getInnerText : c -> string
+
+(**
+   The element immediately following this element. If there is no such
+   element, this returns null.
+*)
+val getNextSiblingElement : c -> c
 
 (**
    The height of an element relative to the layout.
@@ -214,6 +295,14 @@ val getPropertyDouble : c -> string -> float
 val getPropertyInt : c -> string -> int
 
 (**
+   Gets a JSO property from this element.
+  
+   @param name the name of the property to be retrieved
+   @return the property value
+*)
+val getPropertyJSO : c -> string -> JavaScriptObject.c
+
+(**
    Gets a property from this element.
    
    @param name the name of the property to be retrieved
@@ -227,6 +316,14 @@ val getPropertyString : c -> string -> string
 val getScrollHeight : c -> int
 
 (**
+   The number of pixels that an element's content is scrolled from the left.
+   
+   If the element is in RTL mode, this method will return a negative value of
+   the number of pixels scrolled from the right.
+*)
+val getScrollLeft : c -> int
+
+(**
    The number of pixels that an element's content is scrolled from the top.
  *)
 val getScrollTop : c -> int
@@ -237,9 +334,78 @@ val getScrollTop : c -> int
 val getScrollWidth : c -> int
 
 (**
+   Gets a string representation of this element (as outer HTML).
+   
+   We do not override {@link #toString()} because it is final in
+   {@link com.google.gwt.core.client.JavaScriptObject}.
+   
+   @return the string representation of this element
+*)
+val getString : c -> string
+
+(**
+   Gets the element's full tag name, including the namespace-prefix if
+   present.
+   
+   @return the element's tag name
+*)
+val getTagName : c -> string
+
+(**
+   Determines whether an element has an attribute with a given name.
+  
+   Note that IE, prior to version 8, will return false-positives for names
+   that collide with element properties (e.g., style, width, and so forth).
+   
+   @param name the name of the attribute
+   @return <code>true</code> if this element has the specified attribute
+*)
+val hasAttribute : c -> string -> bool
+
+(**
+  Determines whether this element has the given tag name.
+  
+  @param tagName the tag name, including namespace-prefix (if present)
+  @return <code>true</code> if the element has the given tag name
+*)
+val hasTagName : c -> string -> bool
+
+(**
+   Removes a name from this element's class property. If the name is not
+   present, this method has no effect.
+   
+   @param className the class name to be added
+   see {!setClassName(String)}
+*)
+val removeClassName : c -> string -> unit
+
+(**
+  Replace one class name with another.
+ 
+  @param oldClassName the class name to be replaced
+  @param newClassName the class name to replace it
+*)
+val replaceClassName : c -> string -> string -> unit
+
+(**
+   Scrolls this element into view.
+   
+   This method crawls up the DOM hierarchy, adjusting the scrollLeft and
+   scrollTop properties of each scrollable element to ensure that the
+   specified element is completely in view. It adjusts each scroll position by
+   the minimum amount necessary.
+*)
+val scrollIntoView : c -> unit
+
+(**
    All of the markup and content within a given element.
  *)
 val setInnerHTML : c -> string -> unit
+
+(**
+   The text between the start and end tags of the object.
+*)
+val setInnerText : c -> string -> unit
 
 (**
    Sets a boolean property on this element.
@@ -266,6 +432,14 @@ val setPropertyDouble : c -> string -> float -> unit
 val setPropertyInt : c -> string -> int -> unit
 
 (**
+   Sets a JSO property on this element.
+  
+   @param name the name of the property to be set
+   @param value the new property value
+*)
+val setPropertyJSO : c -> string -> JavaScriptObject.c -> unit
+
+(**
    Sets a property on this element.
    
    @param name the name of the property to be set
@@ -274,7 +448,11 @@ val setPropertyInt : c -> string -> int -> unit
 val setPropertyString : c -> string -> string -> unit
 
 (**
+   The number of pixels that an element's content is scrolled to the left.
+*)
+val setScrollLeft : c -> int -> unit
+
+(**
    The element's advisory title.
  *)
 val setTitle : c -> string -> unit
-
