db5dc6408d44ee883467df9a48005db930210c3b
Jake Donham <jake@donham.org>
2010-05-20 13:31:11 -0400

    stub out some java.lang.String methods

---
+++ src/ojox/javaString.mli
@@ -0,0 +1,26 @@
+(*
+ * This file is part of ojox, a library for web browser programming
+ * Copyright (C) 2010 Jacob Donham
+ * Original file (user/super/com/google/gwt/emul/java/lang/String.java
+ * in the GWT source distribution) is Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *)
+
+(** Enough java.lang.String functions to make porting straightforward. *)
+
+val trim : string -> string
+val length : string -> int
+val charAt : string -> int -> char
+val indexOf : string -> ?startIndex:int -> string -> int
+val substring : string -> ?endIndex:int -> int -> string
