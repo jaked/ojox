76a29bdd026322af9e25ef340182b255575d1b43
Jake Donham <jake@donham.org>
2010-06-14 08:10:57 -0400

    Impl

---
+++ src/ojox/impl.ml
@@ -0,0 +1,28 @@
+(*
+ * This file is part of ojox, a library for web browser programming
+ * Copyright (C) 2009 Jacob Donham
+ * Original file (user/src/com/google/gwt/core/client/impl/Impl.java
+ * in the GWT source distribution) is Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *)
+
+open Ocamljs.Inline
+
+let sNextHashId = ref 0
+
+let getNextHashId () =
+  incr sNextHashId;
+  !sNextHashId
+
+let getHashCode o = << o.$$H || (o.$$H = $getNextHashId ()$) >>
